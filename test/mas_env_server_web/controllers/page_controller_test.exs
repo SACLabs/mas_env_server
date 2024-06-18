e07644d01e796d96c9670ab3784a3ac143399e17
陈亮 <PJLAB\chenliang@pjnl104220095l.pjlab.org>
2024-06-18 15:17:09 +0800

    ci normal

--- test/mas_env_server_web/controllers/page_controller_test.exs
+++ test/mas_env_server_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MasEnvServerWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 300) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
   end
 end

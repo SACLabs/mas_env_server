c484a11b3f4f3f708c46a6d516dd292d35686591
陈亮 <PJLAB\chenliang@pjnl104220095l.pjlab.org>
2024-06-19 11:01:50 +0800

    add backend router

--- lib/mas_env_server_web/controllers/page_controller.ex
+++ lib/mas_env_server_web/controllers/page_controller.ex
@@ -1,5 +1,6 @@
 defmodule MasEnvServerWeb.PageController do
   use MasEnvServerWeb, :controller
+  import UUID
 
   def home(conn, _params) do
     # The home page is often custom made,
@@ -7,15 +8,26 @@ defmodule MasEnvServerWeb.PageController do
     render(conn, :home, layout: false)
   end
 
-  def demand(conn, _params) do
-
+  def demand_upload(conn, %{"README" => readme_content, "tests" => test_map}) do
+    # upload a readme file and a test map
+    uuid = UUID.uuid4()
+    # TODO 此处下一个pr完成，将数据存放到env_server的State中
+    conn
+    |> put_status(:ok)
+    |> json(%{"uuid" => uuid})
   end
 
-  def mas_push_report(conn, _params) do
-
+  def mas_push_src(conn, %{"task_id" => task_id, "content" => content}) do
+    # TODO 下一个pr完成对数据的CI/CD过程
+    conn
+    |> put_status(:ok)
+    |> json(%{})
   end
 
-  def get_mas_report_by_id(conn, _params) do
-
+  def get_mas_report_by_id(conn, %{"task_id" => task_id}) do
+    # TODO 下一个pr完成给定task_id返回对应的CI/CD结果
+    conn
+    |> put_status(:ok)
+    |> json(%{})
   end
 end
